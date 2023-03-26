54b10a4fdd91bb197aded5185af59b616829332c
Johnny <johnny_s.o@hotmail.com>
2023-03-25 22:22:58 -0300

    CRiando o IconLogo e importanto no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
+import './components/IconLogo/index'
+import IconLogo from './components/IconLogo/index'
 
 const App = () => {
   (
-    <h1>
-      Hello World
-    </h1>
+    <IconLogo />
   )
 }
 
---
+++ src/components/IconLogo/index.js
@@ -0,0 +1,7 @@
+import { isStyledComponent } from "styled-components";
+
+const IconLogo = styled.img`
+
+`
+
+export default IconLogo;
