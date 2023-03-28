55dc86ad76cfb34c4082178e858782f62b8d90a1
Johnny <johnny_s.o@hotmail.com>
2023-03-28 08:22:14 -0300

    Adicionado a fonte em todo o body

--- src/GlobalStyles/Elements/Base/index.js
+++ src/GlobalStyles/Elements/Base/index.js
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const Base = createGlobalStyle`
   body{ 
     background-color: var(--color-green);
-    }
+    font-family: 'mago3',  sans-serif;
+  }
 `
 export default Base;
