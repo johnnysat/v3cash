b806fba00a9c40dd6978c7eefd737bb23090960a
Johnny <johnny_s.o@hotmail.com>
2023-03-29 22:17:14 -0300

    Adicionado font-face na Base para reconhecer a fonte Mago2

--- src/GlobalStyles/Elements/Base/index.js
+++ src/GlobalStyles/Elements/Base/index.js
@@ -5,6 +5,15 @@ const Base = createGlobalStyle`
     font-size: 10px;
   }
 
+  @font-face {
+        font-family: 'mago3';
+        src: url('fonts/mago3.woff2') format('woff2'),
+            url('fonts/mago3.woff') format('woff');
+        font-weight: 500;
+        font-style: normal;
+        font-display: swap;
+    }
+
   body { 
     background-color: var(--color-green);
     font-family: 'mago3', sans-serif;
