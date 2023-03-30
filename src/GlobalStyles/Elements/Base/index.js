aedbab7d2b3ea083216985e7fb5bbf57753a547e
Johnny <johnny_s.o@hotmail.com>
2023-03-29 22:17:34 -0300

    Adicionado a fonte em font-familly no body da base

--- src/GlobalStyles/Elements/Base/index.js
+++ src/GlobalStyles/Elements/Base/index.js
@@ -6,13 +6,13 @@ const Base = createGlobalStyle`
   }
 
   @font-face {
-        font-family: 'mago3';
-        src: url('fonts/mago3.woff2') format('woff2'),
-            url('fonts/mago3.woff') format('woff');
-        font-weight: 500;
-        font-style: normal;
-        font-display: swap;
-    }
+    font-family: 'mago3';
+    src: url('fonts/mago3.woff2') format('woff2'),
+        url('fonts/mago3.woff') format('woff');
+    font-weight: 500;
+    font-style: normal;
+    font-display: swap;
+  }
 
   body { 
     background-color: var(--color-green);
