d3879436c9e7e2b4e22ba3172e38029f9badfbca
Johnny <johnny_s.o@hotmail.com>
2023-03-31 20:38:33 -0300

    Estilizando Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,7 +4,9 @@ const Button = styled.button`
   background-color: var(--color-sand);
   border: none;
   font-family: inherit;
-  font-size: 1.2rem;
+  font-size: 2rem;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
 `
 
 export default Button
