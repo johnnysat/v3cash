c8c27fe12405bf516a0d9db528a2c589435f431c
Johnny <johnny_s.o@hotmail.com>
2023-03-31 20:37:13 -0300

    Adicionado font-familly e font-size ao button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 const Button = styled.button`
   background-color: var(--color-sand);
   border: none;
+  font-family: inherit;
+  font-size: 1.2rem;
 `
 
 export default Button
